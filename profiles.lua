fbcb70077d46014939ac261f20cbe5414aff1d63
Hjalmar Jakobsson <hjalmar.jakobsson@telia.com>
2021-07-21 18:55:25 +0200

    add doom and neovhy to profiles

--- profiles.lua
+++ profiles.lua
@@ -1,37 +1,47 @@
 --[[
-     /  /\         /__/\         /  /\         /  /\          ___        ___          /__/\
-    /  /:/         \  \:\       /  /:/_       /  /::\        /__/\      /  /\        |  |::\
-   /  /:/           \__\:\     /  /:/ /\     /  /:/\:\       \  \:\    /  /:/        |  |:|:\
-  /  /:/  ___   ___ /  /::\   /  /:/ /:/_   /  /:/  \:\       \  \:\  /__/::\      __|__|:|\:\
- /__/:/  /  /\ /__/\  /:/\:\ /__/:/ /:/ /\ /__/:/ \__\:\  ___  \__\:\ \__\/\:\__  /__/::::| \:\
- \  \:\ /  /:/ \  \:\/:/__\/ \  \:\/:/ /:/ \  \:\ /  /:/ /__/\ |  |:|    \  \:\/\ \  \:\~~\__\/
-  \  \:\  /:/   \  \::/       \  \::/ /:/   \  \:\  /:/  \  \:\|  |:|     \__\::/  \  \:\
-   \  \:\/:/     \  \:\        \  \:\/:/     \  \:\/:/    \  \:\__|:|     /__/:/    \  \:\
-    \  \::/       \  \:\        \  \::/       \  \::/      \__\::::/      \__\/      \  \:\
-     \__\/         \__\/         \__\/         \__\/           ~~~~                   \__\/
+/  /\         /__/\         /  /\         /  /\          ___        ___          /__/\
+/  /:/         \  \:\       /  /:/_       /  /::\        /__/\      /  /\        |  |::\
+/  /:/           \__\:\     /  /:/ /\     /  /:/\:\       \  \:\    /  /:/        |  |:|:\
+/  /:/  ___   ___ /  /::\   /  /:/ /:/_   /  /:/  \:\       \  \:\  /__/::\      __|__|:|\:\
+/__/:/  /  /\ /__/\  /:/\:\ /__/:/ /:/ /\ /__/:/ \__\:\  ___  \__\:\ \__\/\:\__  /__/::::| \:\
+\  \:\ /  /:/ \  \:\/:/__\/ \  \:\/:/ /:/ \  \:\ /  /:/ /__/\ |  |:|    \  \:\/\ \  \:\~~\__\/
+\  \:\  /:/   \  \::/       \  \::/ /:/   \  \:\  /:/  \  \:\|  |:|     \__\::/  \  \:\
+\  \:\/:/     \  \:\        \  \:\/:/     \  \:\/:/    \  \:\__|:|     /__/:/    \  \:\
+\  \::/       \  \:\        \  \::/       \  \::/      \__\::::/      \__\/      \  \:\
+\__\/         \__\/         \__\/         \__\/           ~~~~                   \__\/
 
-	A config switcher written in Lua by NTBBloodbath and Vhyrro.
+A config switcher written in Lua by NTBBloodbath and Vhyrro.
 --]]
 
 -- Defines the profiles you want to use
 local profiles = {
-	--[[
-	Here's an example:
+    --[[
+    Here's an example:
 
-		<name_of_config> = { <path_to_config>, {
-				plugins = "packer", -- Where to install plugins under site/pack
-				preconfigure = "packer:opt" -- Whether or not to preconfigure a plugin manager for you
-			}
-		}
+    <name_of_config> = { <path_to_config>, {
+            plugins = "packer", -- Where to install plugins under site/pack
+            preconfigure = "packer:opt" -- Whether or not to preconfigure a plugin manager for you
+        }
+    }
 
-	More in-depth information can be found in cheovim's README on GitHub.
-	--]]
-	my_config = { "~/.config/nvim.bak", {
-			plugins = "packer",
-			preconfigure = "packer",
-		}
-	},
+    More in-depth information can be found in cheovim's README on GitHub.
+    --]]
+    molleweide = { "~/.config/nvim.config/molleweide", {
+            plugins = "packer",
+            preconfigure = "packer",
+        }
+    },
+    doom_nvim = { "~/.config/nvim.config/doom-nvim", {
+            plugins = "packer",
+            preconfigure = "doom-nvim"
+        }
+    },
+    neovhy = { "~/.config/nvim.config/neovhy", {
+            plugins = "packer",
+            preconfigure = "neovhy",
+        }
+    },
 }
 
 -- return <name_of_config>, <list_of_profiles>
-return "my_config", profiles
+return "molleweide", profiles
